66462a4f145ba4060b758bb190725068d3bde63c
Lev Tsybikov <short10short@gmail.com>
2020-12-10 04:42:59 -0800

    Deleted the  portfolio.js file as it was extra. Added formspree form. deleted previous form button and all related JS code

--- js/portfolio.js
+++ js/portfolio.js
@@ -23,11 +23,3 @@ $(document).ready(function(){
     });
 });
 
-// These functions open and close the contact form
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-    this.Close();
-}
